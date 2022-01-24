6eddc607bf33d9264a30bf712919aff04c166a94
Krzysztof Piotrowski <krzysztof.piotrowski@sollers.eu>
2022-01-24 01:18:07 +0100

    All done. countPrimes used in every method

---
+++ primeNumbrs/primeNumbrs.cpp
@@ -0,0 +1,277 @@
+﻿#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+#include <cmath>
+
+using namespace std;
+
+int isPrime(int number);
+int countPrimes(bool*& arrayBoolean, int arraySize);
+void initializeArrayBoolean(bool*& arrayBoolean, int arraySize, int min, int max);
+void computeDividers(bool*& dividers, int dividersSize);
+
+int metodaDzielacaSekwencyjna(bool*& arrayBoolean, int arraySize, int min);
+int metodaDzielacaRownolegla(bool*& arrayBoolean, int arraySize, int min);
+int metodaSitaSekwencyjna(bool*& arrayBoolean, int arraySize, int min, int max);
+int metodaSitaRownoleglaDomenowa(bool*& arrayBoolean, int arraySize, int min, int max);
+int metodaSitaRownoleglaFunkcyjna(bool*& arrayBoolean, int arraySize, int min, int max);
+
+void printArrayBoolean(bool* arrayBoolean, int arraySize);
+int* convertBoolToIntPrimeArray(bool* arrayBoolean, int arraySize, int primeCount, int min);
+void printArrayInt(int* arrayInt, int arraySize);
+
+int main(int argc, char* argv[])
+{
+	const int min = 0;
+	const int max = 100'000;
+	const int arraySize = max - min + 1;
+	bool* arrayBoolean = new bool[arraySize];
+
+
+	//TIME COUNT
+	clock_t duration = clock();
+
+	//int primeArraySize = metodaDzielacaSekwencyjna(arrayBoolean, arraySize, min);
+	//int primeArraySize = metodaDzielacaRownolegla(arrayBoolean, arraySize, min);
+	//int primeArraySize = metodaSitaSekwencyjna(arrayBoolean, arraySize, min, max);
+	//int primeArraySize = metodaSitaRownoleglaDomenowa(arrayBoolean, arraySize, min, max);
+	int primeArraySize = metodaSitaRownoleglaFunkcyjna(arrayBoolean, arraySize, min, max);
+
+	duration = clock() - duration;
+	printf("It took me %d clicks (%f seconds).\n", duration, ((float)duration) / CLOCKS_PER_SEC);
+	//TIME COUNT
+
+	int* primeArray = convertBoolToIntPrimeArray(arrayBoolean, arraySize, primeArraySize, min);
+	printf("Licznik liczb pierwszych w przedziale <%d;%d>: %d\n", min, max, primeArraySize);
+	//printArrayInt(primeArray, primeArraySize);
+}
+
+int isPrime(int number) {
+	if (number <= 1)
+		return false;
+	for (int i = 2; i <= sqrt(number); i++)
+		if (number % i == 0)
+			return false;
+	return true;
+}
+int countPrimes(bool*& arrayBoolean, int arraySize) {
+	int primeCounter = 0;
+#pragma omp parallel
+	{
+		int localPrimeCounter = 0;
+#pragma omp for nowait
+		for (int i = 0; i < arraySize; i++)
+		{
+			if (true == arrayBoolean[i]) {
+				localPrimeCounter++;
+			}//if
+		}//for
+#pragma omp atomic
+		primeCounter += localPrimeCounter;
+	}//pragma omp parallel
+
+	return primeCounter;
+}
+void initializeArrayBoolean(bool*& arrayBoolean, int arraySize, int min, int max) {
+#pragma omp for
+	for (int i = 0; i < arraySize; i++) {
+		arrayBoolean[i] = true;
+	}//for i
+
+	if (0 == min) {
+		arrayBoolean[0] = false;
+		if (max >= 1) {
+			arrayBoolean[1] = false;
+		}//if2
+	}//if1
+	if (1 == min) {
+		arrayBoolean[0] = false;
+	}//if
+}
+void computeDividers(bool*& dividers, int dividersSize) {
+	for (int i = 0; i < dividersSize; i++) {
+		dividers[i] = true;
+	}
+
+	if (dividersSize > 0)
+		dividers[0] = false;
+	if (dividersSize > 1)
+		dividers[1] = false;
+
+	// F i l t e r non−prime d i v i d e r s
+	for (int i = 2; i < dividersSize; i++) {
+		for (int j = i * i; j < dividersSize; j = j + i) {
+			if (true == dividers[j]) {
+				dividers[j] = false;
+			}//if
+		}//for j
+	}//for i
+}
+
+int metodaDzielacaSekwencyjna(bool*& arrayBoolean, int arraySize, int min) {
+	for (int i = 0; i < arraySize; i++) {
+		arrayBoolean[i] = isPrime(min + i);
+	}//for
+	return countPrimes(arrayBoolean, arraySize);
+}//metodaDzielacaSekwencyjna()
+int metodaDzielacaRownolegla(bool*& arrayBoolean, int arraySize, int min) {
+#pragma omp parallel
+	{
+#pragma omp for schedule (dynamic, 100)
+		for (int i = 0; i < arraySize; i++) {
+			if (isPrime(min + i)) {
+				arrayBoolean[i] = true;
+			}//if
+			else {
+				arrayBoolean[i] = false;
+			}//else
+		}//for
+	}//pragma omp parallel
+	return countPrimes(arrayBoolean, arraySize);
+}//metodaDzielacaRownolegla()
+int metodaSitaSekwencyjna(bool*& arrayBoolean, int arraySize, int min, int max) {
+	initializeArrayBoolean(arrayBoolean, arraySize, min, max);
+	int dividersSize = (int)sqrt(max) + 1;
+
+	bool* dividers = new bool[dividersSize];
+	computeDividers(dividers, dividersSize);
+
+	// F i l t e r non−prime d i v i d e r s
+	for (int i = 2; i < dividersSize; i++) {
+		for (int j = i * i; j < dividersSize; j = j + i) {
+			if (true == dividers[j]) {
+				dividers[j] = false;
+			}//if
+		}//for j
+	}//for i
+
+	// F i l t e r non−prime numbers
+	for (int i = 2; i < dividersSize; i++) {
+		if (dividers[i]) {
+			int j = i * floor((min - 1) / i);
+			if (j < i) {
+				j += i;
+			}//j
+			while (j + i <= max) {
+				j += i;
+				int index = j - min;
+				if (arrayBoolean[index]) {
+					arrayBoolean[index] = false;
+				}//if
+			}//while
+		}//if dividers
+	}//for i
+	return countPrimes(arrayBoolean, arraySize);
+}
+int metodaSitaRownoleglaDomenowa(bool*& arrayBoolean, int arraySize, int min, int max) {
+	initializeArrayBoolean(arrayBoolean, arraySize, min, max);
+	int dividersSize = (int)sqrt(max) + 1;
+
+#pragma omp parallel
+	{
+		//initialize dividers
+		bool* dividers = new bool[dividersSize];
+		computeDividers(dividers, dividersSize);
+
+		int threadMax = omp_get_max_threads();//
+		int threadId = omp_get_thread_num();//
+		int minIndex = min + threadId * arraySize / threadMax;//
+		int maxIndex = min + (threadId + 1) * arraySize / threadMax;//
+
+		// F i l t e r non−prime numbers
+		for (int i = 2; i < dividersSize; i++) {
+			if (dividers[i]) {
+				int j = i * floor((minIndex - 1) / i);//
+				if (j < i) {
+					j += i;
+				}//j
+				while (j + i <= maxIndex) {//
+					j += i;
+					int index = j - min;
+					if (arrayBoolean[index]) {
+						arrayBoolean[index] = false;
+					}//if
+				}//while
+			}//if dividers
+		}//for i
+	}//pragma omp parallel
+
+	return countPrimes(arrayBoolean, arraySize);
+}
+int metodaSitaRownoleglaFunkcyjna(bool*& arrayBoolean, int arraySize, int min, int max) {
+	initializeArrayBoolean(arrayBoolean, arraySize, min, max);
+	int dividersSize = (int)sqrt(max) + 1;
+
+	bool* dividers = new bool[dividersSize];
+	computeDividers(dividers, dividersSize);
+
+	// F i l t e r non−prime d i v i d e r s
+	for (int i = 2; i < dividersSize; i++) {
+		for (int j = i * i; j < dividersSize; j = j + i) {
+			if (true == dividers[j]) {
+				dividers[j] = false;
+			}//if
+		}//for j
+	}//for i
+
+#pragma omp parallel
+	{
+		int localNonPrimeCount = 0;
+		// F i l t e r non−prime numbers
+#pragma omp for schedule(dynamic)
+		for (int i = 2; i < dividersSize; i++) {
+			if (dividers[i]) {
+				int j = i * floor((min - 1) / i);
+				if (j < i) {
+					j += i;
+				}//j
+				while (j + i <= max) {
+					j += i;
+					int index = j - min;
+					if (arrayBoolean[index]) {
+						arrayBoolean[index] = false;
+					}//if
+				}//while
+			}//if dividers
+		}//for i
+	}//pragma omp parallel
+	return countPrimes(arrayBoolean, arraySize);
+}
+
+void printArrayBoolean(bool* arrayBoolean, int arraySize)
+{
+	for (int i = 0; i < arraySize; i++) {
+		//Next line after each 10 values
+		if (i % 10 == 0)
+			printf("\n");
+		//Print values with ", " excluding endling for last value
+		printf("%d", arrayBoolean[i]);
+		if (i < arraySize - 1)
+			printf(",\t");
+	}
+	printf("\n");
+}
+void printArrayInt(int* arrayInt, int arraySize)
+{
+	for (int i = 0; i < arraySize; i++) {
+		//Next line after each 10 values
+		if (i % 10 == 0)
+			printf("\n");
+		//Print values with ", " excluding endling for last value
+		printf("%d", arrayInt[i]);
+		if (i < arraySize - 1)
+			printf(",\t");
+	}
+	printf("\n");
+}
+int* convertBoolToIntPrimeArray(bool* arrayBoolean, int arraySize, int primeCount, int min) {
+	int* resultArray = new int[primeCount];
+	int primeIndex = 0;
+	for (int i = 0; i < arraySize; i++) {
+		if (true == arrayBoolean[i]) {
+			resultArray[primeIndex] = i + min;
+			primeIndex++;
+		}//if
+	}//for
+	return resultArray;
+}
